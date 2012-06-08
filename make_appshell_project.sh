7f14c8857b581406531fcd5e6425abd259e0bf9e
Glenn Ruehle <gruehle@gmail.com>
2012-06-08 14:58:08 -0700

    Fix tool paths in mac project.

--- make_appshell_project.sh
+++ make_appshell_project.sh
@@ -9,3 +9,7 @@ fi
 # Build the project
 echo "Building xcode project."
 $CHROMIUM_SRC_PATH/tools/gyp/gyp appshell.gyp -I $CHROMIUM_SRC_PATH/build/common.gypi --depth=$CHROMIUM_SRC_PATH/
+
+# Fix up paths
+sed 's:[^ ]*/src/build/mac/:tools/:' appshell.xcodeproj/project.pbxproj > tmp_pbxproj.txt
+mv tmp_pbxproj.txt appshell.xcodeproj/project.pbxproj
