ad9b0e3ed77f70ac200a95ee7bededd496fccb06
Jason San Jose <jasonsj@adobe.com>
2012-08-14 15:42:25 -0700

    implement brackets.app.language on windows. change API to return wstring

--- appshell/client_app_win.cpp
+++ appshell/client_app_win.cpp
@@ -29,6 +29,32 @@
 
 extern DWORD g_appStartupTime;
 
+std::wstring ClientApp::GetCurrentLanguage()
+{
+	// Get the user's selected language
+	// Defaults to the system installed language if not using MUI.
+	LANGID langID = GetUserDefaultUILanguage();
+
+	// Convert LANGID to a RFC 4646 language tag (per navigator.language)
+	int langSize = GetLocaleInfo(langID, LOCALE_SISO639LANGNAME, NULL, 0);
+	int countrySize = GetLocaleInfo(langID, LOCALE_SISO3166CTRYNAME, NULL, 0);
+
+	wchar_t *lang = new wchar_t[langSize + countrySize + 1];
+	wchar_t *country = new wchar_t[countrySize];
+	
+	GetLocaleInfo(LOCALE_SYSTEM_DEFAULT, LOCALE_SISO639LANGNAME, lang, langSize);
+	GetLocaleInfo(LOCALE_SYSTEM_DEFAULT, LOCALE_SISO3166CTRYNAME, country, countrySize);
+
+	// add hyphen
+	wcscat(wcscat(lang, L"-"), country);
+	std::wstring locale(lang);
+
+	delete [] lang;
+	delete [] country;
+
+	return locale;
+}
+
 std::string ClientApp::GetExtensionJSSource()
 {
     extern HINSTANCE hInst;
