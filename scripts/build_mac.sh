386a93144bb63ef8f2b31cf1f1a4153c50247a32
Narciso Jaramillo <njaramil@adobe.com>
2012-08-31 16:46:51 -0700

    Change build scripts to get the app name and branches from environment variables

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -6,25 +6,39 @@
 #   (copy CEF binary, run scripts/make_symlinks.sh, etc.)
 # - Set BRACKETS_SRC environment variable, pointing to the
 #   brackets source code (without trailing '/')
+# - Optionally, set BRACKETS_APP_NAME environment variable with the full
+#   name of the application.
+# - Optionally, set BRACKETS_SHELL_BRANCH and BRACKETS_BRANCH
+#   to the branches you want to build.
 
 # Make sure BRACKETS_SRC environment variable is set
 if [ "$BRACKETS_SRC" = "" ]; then
-  echo "BRACKETS_SRC environment variable is not set. Aborting."
+  echo "The BRACKETS_SRC environment variable must be set to the location of the Brackets source folder. Aborting."
   exit
 fi
 
-# Set this to the branch you want to build from
-branchName=master
+# Default the app name to "Brackets", but override with $BRACKETS_APP_NAME if set
+if [ "$BRACKETS_APP_NAME" = "" ]; then
+  export BRACKETS_APP_NAME="Brackets"
+fi
+
+# Default the branches to "master"
+if [ "$BRACKETS_SHELL_BRANCH" = "" ]; then
+  export BRACKETS_SHELL_BRANCH="master"
+fi
+if [ "$BRACKETS_BRANCH" = "" ]; then
+  export BRACKETS_BRANCH="master"
+fi
 
 # Pull the latest code
 curDir=`pwd`
-cd $BRACKETS_SRC
-git checkout $branchName
-git pull origin $branchName
+cd "$BRACKETS_SRC"
+git checkout "$BRACKETS_BRANCH"
+git pull origin "$BRACKETS_BRANCH"
 git submodule update --init --recursive
 cd $curDir
-git checkout $branchName
-git pull origin $branchName
+git checkout "$BRACKETS_SHELL_BRANCH"
+git pull origin "$BRACKETS_SHELL_BRANCH"
 
 # Clean and build the xcode project
 xcodebuild -project appshell.xcodeproj -config Release clean
@@ -35,14 +49,13 @@ scripts/package_www_files.sh
 
 # Remove existing staging dir
 if [ -d installer/mac/staging ]; then
-  rm -rf installer/mac/staging/*
-  rmdir installer/mac/staging
+  rm -rf installer/mac/staging
 fi
 
 mkdir installer/mac/staging
 
 # Copy to installer staging folder
-cp -R xcodebuild/Release/Brackets.app installer/mac/staging/
+cp -R "xcodebuild/Release/${BRACKETS_APP_NAME}.app" installer/mac/staging/
 
 # Build the installer
 cd installer/mac
