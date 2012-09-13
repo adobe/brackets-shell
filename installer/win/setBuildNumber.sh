3ef74afbf86bc89574e18fb30df4d65200e36d89
Glenn Ruehle <gruehle@gmail.com>
2012-09-13 13:15:30 -0700

    Bash script for injecting build number.

---
+++ installer/win/setBuildNumber.sh
@@ -0,0 +1,33 @@
+# Injects the build number, branch, and SHA into the staged package.json file.
+#
+# INSTRUCTIONS FOR RUNNING
+# ------------------------
+# 1. This script MUST be run from the Git Bash shell
+#    This script should be run *after* running stageForInstaller.bat
+# 2. Set the BRACKETS_SRC environment variable to point to the location
+#    of the brackets source folder. 
+#    e.g. export BRACKETS_SRC=/c/Users/glenn/dev/brackets
+# 3. cd to the root of the brackets-shell repo
+# 4. Run installer/win/setBuildNumber.sh
+
+
+# Make sure BRACKETS_SRC environment variable is set
+if [ "$BRACKETS_SRC" = "" ]; then
+  echo "The BRACKETS_SRC environment variable must be set to the location of the Brackets source folder. Aborting."
+  exit
+fi
+
+curDir=`pwd`
+cd "$BRACKETS_SRC"
+build_num=`git log --oneline | wc -l | tr -d ' '`
+branch_name=`git status | head -1 | sed -e 's/# On branch \(.*\)/\1/'`
+brackets_sha=`git log | head -1 | sed -e 's/commit \([0-9a-f]*$\)/\1/'`
+cd $curDir
+
+# Set the build number, branch and sha on the staged build
+cat "installer/win/staging/www/package.json" \
+|   sed "s:\(\"version\"[^\"]*\"[0-9.]*-\)\([0-9*]\)\(\"\):\1$build_num\3:" \
+|   sed "s:\(\"branch\"[^\"]*\"\)\([^\"]*\)\(\"\):\1$branch_name\3:" \
+|   sed "s:\(\"SHA\"[^\"]*\"\)\([^\"]*\)\(\"\):\1$brackets_sha\3:" \
+> tmp_package_json.txt
+mv tmp_package_json.txt "installer/win/staging/www/package.json"
