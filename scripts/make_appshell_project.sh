c9ac87ebef29c40b986075b1ff8218f820d1796d
Glenn Ruehle <gruehle@gmail.com>
2012-12-12 13:44:19 -0800

    Add gyp tool.

--- scripts/make_appshell_project.sh
+++ scripts/make_appshell_project.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-# Check for CHROMIUM_SRC_PATH
-if [ "$CHROMIUM_SRC_PATH" = "" ]; then
-  echo "CHROMIUM_SRC_PATH environment variable is not set. Aborting."
-  exit
-fi
-
 # Build the project
 echo "Building xcode project."
-$CHROMIUM_SRC_PATH/tools/gyp/gyp appshell.gyp -I $CHROMIUM_SRC_PATH/build/common.gypi --depth=$CHROMIUM_SRC_PATH/
+gyp/gyp appshell.gyp -I common.gypi --depth=.
 
 # Fix up paths
-sed 's:[^ ]*/src/build/mac/:tools/:' appshell.xcodeproj/project.pbxproj > tmp_pbxproj.txt
+sed 's:exec mac/:exec tools/:' appshell.xcodeproj/project.pbxproj > tmp_pbxproj.txt
 mv tmp_pbxproj.txt appshell.xcodeproj/project.pbxproj
 
 sed 's:SYMROOT = \([^\;]*\);:SYMROOT = xcodebuild;:' appshell.xcodeproj/project.pbxproj > tmp_pbxproj.txt
