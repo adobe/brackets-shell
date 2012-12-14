9ee7a29f1410eba9584f60ae85ad06ec0bce2687
Glenn Ruehle <gruehle@gmail.com>
2012-12-14 14:05:17 -0800

    Use junctions instead of symlinks on Windows.

--- scripts/make_symlinks.bat
+++ scripts/make_symlinks.bat
@@ -1,10 +1,11 @@
 REM Remove existing links
-rmdir Debug include lib libcef_dll Release tools
+rmdir Debug include lib libcef_dll Release
 
 REM Make new links
-mklink /d Debug deps\cef\Debug
-mklink /d include deps\cef\include
-mklink /d lib deps\cef\lib
-mklink /d libcef_dll deps\cef\libcef_dll
-mklink /d Release deps\cef\Release
-mklink /d tools deps\cef\tools
+mklink /j Debug deps\cef\Debug
+mklink /j include deps\cef\include
+mklink /j lib deps\cef\lib
+mklink /j libcef_dll deps\cef\libcef_dll
+mklink /j Release deps\cef\Release
+
+exit
