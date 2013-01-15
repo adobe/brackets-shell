b4a2ae1d8ee109ebd34c9e078bbe694002000766
Joel Brandt <joelrbrandt@gmail.com>
2013-01-14 17:10:13 -0800

    check for vs2012 first

--- scripts/build_projects.bat
+++ scripts/build_projects.bat
@@ -2,19 +2,19 @@
 :: This file should *not* be called directly. It is called by the build.sh script.
 ECHO OFF
 
-IF DEFINED VS100COMNTOOLS GOTO VS2010
 IF DEFINED VS110COMNTOOLS GOTO VS2012
+IF DEFINED VS100COMNTOOLS GOTO VS2010
 
 :VSNotInstalled
 ECHO Visual Studio 2010 or 2012 must be installed.
 GOTO Exit
 
-:VS2010
-call "%VS100COMNTOOLS%/vsvars32.bat"
-GOTO Build
-
 :VS2012
 call "%VS110COMNTOOLS%/vsvars32.bat"
+GOTO Build
+
+:VS2010
+call "%VS100COMNTOOLS%/vsvars32.bat"
 GOTO Build
 
 :Build
