1d51ed78e57765dab1703edd85dc655744222efc
Randy Edmunds <redmunds@adobe.com>
2013-03-08 16:00:32 -0800

    always enable remote debugging

--- appshell/appshell_extensions.cpp
+++ appshell/appshell_extensions.cpp
@@ -292,7 +292,7 @@ public:
             // handler->ShowDevTools(browser);
             
             ExtensionString url(browser->GetHost()->GetDevToolsURL(true));
-            OpenLiveBrowser(url, false);
+            OpenLiveBrowser(url, true);
 
         } else if (message_name == "GetNodeState") {
             // Parameters:
