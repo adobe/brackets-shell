2ebfe92ba25743c8666b8fee0a3bd5a3e47ad304
Jason San Jose <jasonsj@adobe.com>
2013-06-12 10:25:01 -0700

    clean up linux setup script

--- scripts/setup_linux_build.sh
+++ scripts/setup_linux_build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# SYSTEM DEPENDENCIES
+# install development dependencies
+sudo apt-get install --assume-yes p7zip-full libnss3-1d libnspr4-0d gyp gtk+-2.0
+
+# BUILD DEPENDENCIES
+# download CEF
 zipname="cef_binary_3.1453.1255_linux32"
 
 mkdir deps
 pushd deps
 
-# download CEF
 if [ ! -f cef.zip ]; then
   wget -O cef.7z "https://drive.google.com/uc?export=download&id=0B7as0diokeHxMVpTOTM5NUJwemM"
 fi
@@ -15,34 +20,22 @@ rm -rf cef
 7za x cef.7z
 mv $zipname cef
 
-# get libudev0 for Ubuntu 13.04
-if egrep -q "13\.04" /etc/issue; then
-  if [ ! -f libudev0.deb ]; then
-    wget -O libudev0.deb http://launchpadlibrarian.net/132294322/libudev0_175-0ubuntu19_i386.deb
-  fi
-
-  sudo dpkg -i *.deb 
-  sudo apt-get install -f
-fi
-
-# download chromium dependency script
-if [ ! -f install-build-deps.sh ]; then
-  wget -O install-build-deps.sh https://gist.github.com/jasonsanjose/5514813/raw/8f333053a1a1180c2bfd4aabca94d4cff76e8595/install-build-deps.sh
-  chmod 755 install-build-deps.sh
-fi
-
-# install dependecies
-./install-build-deps.sh --no-chromeos-fonts
-
 popd
 
 # make symlinks to CEF
-./scripts/make_symlinks.sh
+# Remove existing links
+rm -f Debug include libcef_dll Release Resources tools
 
-# install and run gyp to create makefile
-sudo apt-get install gyp -qq --assume-yes
+# Make new links
+# ln -s deps/cef/Debug/ Debug
+ln -s deps/cef/include/ include
+ln -s deps/cef/libcef_dll/ libcef_dll
+ln -s deps/cef/Release/ Release
+ln -s deps/cef/Resources/ Resources
+# ln -s deps/cef/tools/ tools
 
-# copy appshell.gp
+# CREATE MAKEFILE AND BUILD
+# copy appshell.gyp
 cp appshell.gyp.txt appshell.gyp
 
 gyp --depth .
