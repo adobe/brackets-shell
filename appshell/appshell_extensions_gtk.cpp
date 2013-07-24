db37a89c311f47ad798e4005b93a9662b00e5388
Tim Burgess <timburgess@mac.com>
2013-07-24 12:25:07 +1000

    Implemented GTK ShowSaveDialog (for Save As)

--- appshell/appshell_extensions_gtk.cpp
+++ appshell/appshell_extensions_gtk.cpp
@@ -190,7 +190,25 @@ int32 ShowSaveDialog(ExtensionString title,
                      ExtensionString proposedNewFilename,
                      ExtensionString& newFilePath)
 {
-    // TODO
+
+    GtkWidget *dialog = gtk_file_chooser_dialog_new(title.c_str(),
+                                         NULL,
+                                         GTK_FILE_CHOOSER_ACTION_SAVE,
+                                         GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+                                         GTK_STOCK_SAVE, GTK_RESPONSE_ACCEPT,
+                                         NULL);
+    gtk_file_chooser_set_do_overwrite_confirmation(GTK_FILE_CHOOSER(dialog), TRUE);
+    gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(dialog), initialDirectory.c_str());
+    gtk_file_chooser_set_current_name(GTK_FILE_CHOOSER(dialog), proposedNewFilename.c_str());
+    
+    if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
+        char* filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+        newFilePath.assign(filename);
+
+        g_free(filename);
+    }
+
+    gtk_widget_destroy(dialog);
     return NO_ERROR;
 }
 
