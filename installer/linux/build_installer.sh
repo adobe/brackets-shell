0c765dea035326224bbb6970c41f27649fbb9496
Jason San Jose <jasonsj@adobe.com>
2013-08-14 15:22:21 -0700

    fix brackets icon

--- installer/linux/build_installer.sh
+++ installer/linux/build_installer.sh
@@ -3,7 +3,9 @@
 # grunt-contrib-copy doesn't preserve permissions
 # https://github.com/gruntjs/grunt/issues/615
 chmod 775 debian/package-root/opt/brackets/Brackets
-chmod 775 debian/package-root/DEBIAN/postinst
+chmod 755 debian/package-root/DEBIAN/prerm
+chmod 755 debian/package-root/DEBIAN/postrm
+chmod 755 debian/package-root/DEBIAN/postinst
 
 # set permissions on subdirectories
 find debian -type d -exec chmod 755 {} \;
