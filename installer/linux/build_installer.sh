ce99e3868cb0e899a4392ec767cf56984a62985a
Jason San Jose <jasonsj@adobe.com>
2013-08-20 12:12:39 -0700

    fix Brackets-node permissions

--- installer/linux/build_installer.sh
+++ installer/linux/build_installer.sh
@@ -2,8 +2,9 @@
 
 # grunt-contrib-copy doesn't preserve permissions
 # https://github.com/gruntjs/grunt/issues/615
-chmod 755 debian/package-root/opt/brackets/Brackets
 chmod 755 debian/package-root/opt/brackets/brackets
+chmod 755 debian/package-root/opt/brackets/Brackets
+chmod 755 debian/package-root/opt/brackets/Brackets-node
 chmod 755 debian/package-root/DEBIAN/prerm
 chmod 755 debian/package-root/DEBIAN/postrm
 chmod 755 debian/package-root/DEBIAN/postinst
