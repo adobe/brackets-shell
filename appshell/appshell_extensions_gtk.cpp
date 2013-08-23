84ea9a5db3197921191a9544a857e031a1cce57f
Jeff Booher <jsbooher@adobe.com>
2013-08-23 16:27:46 -0700

    fix whitespace

--- appshell/appshell_extensions_gtk.cpp
+++ appshell/appshell_extensions_gtk.cpp
@@ -431,7 +431,7 @@ int32 CopyFile(ExtensionString src, ExtensionString dest)
     	return ConvertLinuxErrorCode(errno);
  
     if ((out_fd = creat(dest.c_str(), COPYMORE)) == -1) {
-	result = ConvertLinuxErrorCode(errno);
+	    result = ConvertLinuxErrorCode(errno);
         close(in_fd);
     	return result;
     }
@@ -441,8 +441,8 @@ int32 CopyFile(ExtensionString src, ExtensionString dest)
     while((n_chars = read(in_fd, buf, BUFFERSIZE)) > 0)
     {
     	if (write(out_fd, buf, n_chars) != n_chars)
-  	{
-	    result = ConvertLinuxErrorCode(errno);
+  	    {
+	        result = ConvertLinuxErrorCode(errno);
       	    close(in_fd);
             close(out_fd);
             unlink(dest.c_str());
@@ -457,7 +457,7 @@ int32 CopyFile(ExtensionString src, ExtensionString dest)
     if (n_chars == -1)
     {
         result = ConvertLinuxErrorCode(errno);
-	unlink(dest.c_str());
+	    unlink(dest.c_str());
         return result;
     }
  
