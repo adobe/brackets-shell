12c8551e3e2ef4c83ddda992b35d681c0ccaa62e
maciek <poczciwy@gmail.com>
2013-08-26 23:08:00 +0200

    documentation

--- installer/linux/build_installer.sh
+++ installer/linux/build_installer.sh
@@ -8,18 +8,19 @@
 
 DISTRO="unknown"
 
+
+# get distribution name from /etc/*release file
 if [[ -f /etc/os-release ]]; then
-    . /etc/os-release
-    DISTRO=$ID
+    . /etc/os-release; DISTRO=$ID
 elif [[ -f /etc/lsb_release ]]; then
-    . /etc/lsb_release
-    DISTRO=$DISTRIB_ID
+    . /etc/lsb_release; DISTRO=$DISTRIB_ID
 else
     echo "Error: non-standard /etc/*release files are currently not supported" 2>&1
     exit 1
 fi;
 
 
+# run distro-specified builder
 if [[ -f ${DISTRO}_builder.sh ]]; then
     bash ${DISTRO}_builder.sh
 else
