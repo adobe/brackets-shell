aa53948322bd1f0ede7eac70828cc2e91f8c0320
maciek <poczciwy@gmail.com>
2013-08-26 12:48:39 +0200

    unsupported distros handling

--- installer/linux/build_installer.sh
+++ installer/linux/build_installer.sh
@@ -12,6 +12,9 @@ if [[ -f /etc/os-release ]]; then
 elif [[ -f /etc/lsb_release ]]; then
     . /etc/lsb_release
     DISTRO=$DISTRIB_ID
+else
+    echo "Error: non-standard /etc/*release file are currently not supported" 2>&1
+    exit 1
 fi;
 
 
@@ -19,4 +22,7 @@ if [[ $DISTRO == "ubuntu" ]]; then
     bash ubuntu_builder.sh
 elif [[ $DISTRO == "arch" ]]; then
     bash arch_builder.sh
+else
+    echo "Error: can't build package for distribution \"${DISTRO}\"" 2>&1
+    exit 1
 fi;
