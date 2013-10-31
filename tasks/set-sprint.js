c61228e9de676439187212f93c3630c84b27f19c
Bryan Chin <bchin@adobe.com>
2013-10-31 18:21:24 -0500

    update grunt set-sprint to remove deprecated Sprint number references.  Also remove step to refresh shortcut guid.

--- tasks/set-sprint.js
+++ tasks/set-sprint.js
@@ -25,8 +25,7 @@
 module.exports = function (grunt) {
     "use strict";
     
-    var guid = require("guid"),
-        common = require("./common")(grunt);
+    var common = require("./common")(grunt);
 
     function safeReplace(content, regexp, replacement) {
         var newContent = content.replace(regexp, replacement);
@@ -43,7 +42,6 @@ module.exports = function (grunt) {
         var packageJsonPath             = "package.json",
             packageJSON                 = grunt.file.readJSON(packageJsonPath),
             winInstallerBuildXmlPath    = "installer/win/brackets-win-install-build.xml",
-            buildInstallerScriptPath    = "installer/mac/buildInstaller.sh",
             wxsPath                     = "installer/win/Brackets.wxs",
             versionRcPath               = "appshell/version.rc",
             infoPlistPath               = "appshell/mac/Info.plist",
@@ -75,25 +73,7 @@ module.exports = function (grunt) {
         );
         grunt.file.write(winInstallerBuildXmlPath, text);
         
-        // 3. Open installer/mac/buildInstaller.sh and change `releaseName`
-        text = grunt.file.read(buildInstallerScriptPath);
-        text = safeReplace(
-            text,
-            /(Brackets Sprint )([0-9]+)/,
-            "$1" + sprint
-        );
-        grunt.file.write(buildInstallerScriptPath, text);
-        
-        // 4. Open installer/win/Brackets.wxs and replace the `StartMenuShortcut` GUID property with a newly generated GUID
-        text = grunt.file.read(wxsPath);
-        text = safeReplace(
-            text,
-            /<Component Id="StartMenuShortcut" Guid="\{[0-9A-Fa-f\-]+\}" Win64="no" >/,
-            '<Component Id="StartMenuShortcut" Guid="{' + guid.raw() + '}" Win64="no" >'
-        );
-        grunt.file.write(wxsPath, text);
-        
-        // 5. Open appshell/version.rc and change `FILEVERSION` and `"FileVersion"`
+        // 3. Open appshell/version.rc and change `FILEVERSION` and `"FileVersion"`
         text = grunt.file.read(versionRcPath);
         text = safeReplace(
             text,
@@ -107,7 +87,7 @@ module.exports = function (grunt) {
         );
         grunt.file.write(versionRcPath, text);
         
-        // 6. Open appshell/mac/Info.plist and change `CFBundleShortVersionString` and `CFBundleVersion`text = grunt.file.read(wxsPath);
+        // 4. Open appshell/mac/Info.plist and change `CFBundleShortVersionString` and `CFBundleVersion`text = grunt.file.read(wxsPath);
         text = grunt.file.read(infoPlistPath);
         text = safeReplace(
             text,
