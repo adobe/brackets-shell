651bfd6c1f2321ddbdbb9767050f19a6769b3efc
RaymondLim <rayblim@gmail.com>
2014-02-10 14:51:07 -0800

    Fix the comment.

--- appshell/appshell_extensions_win.cpp
+++ appshell/appshell_extensions_win.cpp
@@ -604,7 +604,7 @@ int32 ReadDir(ExtensionString path, CefRefPtr<CefListValue>& directoryContents)
     path += '*';
 
     // Convert to native path to ensure that FindFirstFile and FindNextFile
-    // functions correctly for all paths including paths to a network drive.
+    // function correctly for all paths including paths to a network drive.
     ConvertToNativePath(path);
 
     WIN32_FIND_DATA ffd;
