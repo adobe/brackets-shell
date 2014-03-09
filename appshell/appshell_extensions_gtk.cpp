bf345049e5b2562100500a6adb4738b8035b61ce
Max Ehrlich <max.ehr@gmail.com>
2014-03-09 01:54:37 -0500

    Added implementation for realpath Expands symbolic links in GetFileInfo for gtk/linux hosts using posix realpath function

--- appshell/appshell_extensions_gtk.cpp
+++ appshell/appshell_extensions_gtk.cpp
@@ -327,9 +327,7 @@ int GetFileInfo(ExtensionString filename, uint32& modtime, bool& isDir, double& 
     isDir = S_ISDIR(buf.st_mode);
     size = (double)buf.st_size;
     
-    // TODO: Implement realPath. If "filename" is a symlink, realPath should be the actual path
-    // to the linked object.
-    realPath = "";
+    realPath = realpath(filename.c_str(), NULL);
     
     return NO_ERROR;
 }
