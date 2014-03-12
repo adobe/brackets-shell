1a5c0076146ebff76ca8b8ea0c114b8eb32ab24d
Randy Edmunds <redmunds@adobe.com>
2014-03-12 11:10:31 -0700

    update sprint number to 38

--- installer/mac/buildInstaller.sh
+++ installer/mac/buildInstaller.sh
@@ -2,7 +2,7 @@
 
 # config
 releaseName="Brackets"
-dmgName="${releaseName} Sprint 37"
+dmgName="${releaseName} Sprint 38"
 format="bzip2"
 encryption="none"
 tmpLayout="./dropDmgConfig/layouts/tempLayout"
