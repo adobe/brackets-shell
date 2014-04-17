d447b90204fae120b1a560804e8919d0b749913a
Arron Washington <arron@theradicaledwards.com>
2014-04-17 16:56:06 -0400

    Linux uninstall script: optionally remove Brackets config.
    When passed the -c parameter, uninstall.sh will remove the user's local
    Brackets configuration.

--- installer/linux/archive/uninstall.sh
+++ installer/linux/archive/uninstall.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 echo 'uninstalling brackets from your ~/.local directory'
+echo 'run with -c to remove ~/.config/Brackets'
+
+removeConfig=false
+
+while getopts c opt; do
+  case $opt in
+    c)
+      removeConfig=true
+      ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+
+if [ "$removeConfig" = true ]; then
+    echo 'deleting ~/.config/Brackets'
+    rm -rf ~/.config/Brackets
+fi
 
 # Safety first: only uninstall files we know about.
 
