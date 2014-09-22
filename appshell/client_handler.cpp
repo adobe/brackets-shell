73f0bba002c965234969e5124fe5c7be2823fc1c
Jason San Jose <jasonsj@adobe.com>
2014-09-22 11:14:15 -0700

    fix body margin and padding

--- appshell/client_handler.cpp
+++ appshell/client_handler.cpp
@@ -169,7 +169,7 @@ void ClientHandler::OnLoadError(CefRefPtr<CefBrowser> browser,
   ss << "<html>" <<
         "<head>" <<
         "  <style type='text/css'>" <<
-        "    body { background: #3c3f41; width:100%; height:100%; overflow: hidden; }" <<
+        "    body { background: #3c3f41; width:100%; height:100%; margin: 0; padding: 0; }" <<
         "    .logo { background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAMdJREFUeNpi0Vz9SICBgWE9EDswkAYOAHEgE5maGaB61rPANGdr8QMxH1E6p177BMQfwYawwARBmv38/FAUbtq0CUxjE4cawMDEQCFgwSYIs5mQS8AG/P//nyybYfpYGP6T6fb/1HLB/39kGgDVx/L37z+yDIDpY/n35y+GJCy08YU+TB/L719/yXIBTB/L75+/wIyeky8x4h9XugCpheljFOw4vf8/eZmJgRGYI1l+/vgdCPTPemC0kGQIIyPjASYW5kCAAAMA5Oph7ZyIYMQAAAAASUVORK5CYII='); }"
         "    .debug { cursor: hand; position: absolute; bottom: 16px; right: 16px; width: 16px; height: 16px; font-family: sans-serif; font-size: .75em; color: #999; }" <<
         "  </style>" <<
