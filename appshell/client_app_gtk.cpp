17db6f146085d29f3bd58c416bcd73c1bde38151
Jason San Jose <jasonsj@adobe.com>
2014-12-05 11:08:34 -0800

    fix missing gtk dependencies

--- appshell/client_app_gtk.cpp
+++ appshell/client_app_gtk.cpp
@@ -1,31 +1,32 @@
 /*
  * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
- *  
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"), 
- * to deal in the Software without restriction, including without limitation 
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the 
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- *  
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *  
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
- * 
- */ 
+ *
+ */
 
 #include "client_app.h"
 #include "resource.h"
 #include "include/cef_base.h"
 #include "config.h"
 
+#include <gtk/gtk.h>
 #include <pwd.h>
 #include <algorithm>
 //#include <MMSystem.h>
@@ -79,16 +80,16 @@ double ClientApp::GetElapsedMilliseconds()
     return (time(NULL) - g_appStartupTime);
 }
 
-CefString ClientApp::AppGetSupportDirectory() 
+CefString ClientApp::AppGetSupportDirectory()
 {
     gchar *supportDir = g_strdup_printf("%s/%s", g_get_user_config_dir(), APP_NAME);
     CefString result = CefString(supportDir);
     g_free(supportDir);
-    
+
     return result;
 }
 
-CefString ClientApp::AppGetDocumentsDirectory() 
+CefString ClientApp::AppGetDocumentsDirectory()
 {
     const char *dir = g_get_user_special_dir(G_USER_DIRECTORY_DOCUMENTS);
     if (dir == NULL)  {
