99026c1e331d9df8a66d6275f55e060e4a74c142
Ingo Richter <ingo.richter@gmail.com>
2014-12-08 00:23:35 -0800

    - resolved the set window title crash by using the X11 window

--- appshell/client_handler_gtk.cpp
+++ appshell/client_handler_gtk.cpp
@@ -29,12 +29,43 @@ void ClientHandler::OnAddressChange(CefRefPtr<CefBrowser> browser,
 void ClientHandler::OnTitleChange(CefRefPtr<CefBrowser> browser,
                                   const CefString& title) {
   REQUIRE_UI_THREAD();
-
-  GtkWidget* window = gtk_widget_get_ancestor(
-      GTK_WIDGET(browser->GetHost()->GetWindowHandle()),
-      GTK_TYPE_WINDOW);
   std::string titleStr(title);
-  gtk_window_set_title(GTK_WINDOW(window), titleStr.c_str());
+
+  // Retrieve the X11 display shared with Chromium.
+  ::Display* display = cef_get_xdisplay();
+  DCHECK(display);
+
+  // Retrieve the X11 window handle for the browser.
+  ::Window window = browser->GetHost()->GetWindowHandle();
+  DCHECK(window != kNullWindowHandle);
+
+  // Retrieve the atoms required by the below XChangeProperty call.
+  const char* kAtoms[] = {
+    "_NET_WM_NAME",
+    "UTF8_STRING"
+  };
+  Atom atoms[2];
+  int result = XInternAtoms(display, const_cast<char**>(kAtoms), 2, false,
+                            atoms);
+  if (!result)
+    NOTREACHED();
+
+  // Set the window title.
+  XChangeProperty(display,
+                  window,
+                  atoms[0],
+                  atoms[1],
+                  8,
+                  PropModeReplace,
+                  reinterpret_cast<const unsigned char*>(titleStr.c_str()),
+                  titleStr.size());
+
+  // TODO(erg): This is technically wrong. So XStoreName and friends expect
+  // this in Host Portable Character Encoding instead of UTF-8, which I believe
+  // is Compound Text. This shouldn't matter 90% of the time since this is the
+  // fallback to the UTF8 property above.
+  XStoreName(display, browser->GetHost()->GetWindowHandle(), titleStr.c_str());
+
 }
 
 void ClientHandler::SendNotification(NotificationType type) {
