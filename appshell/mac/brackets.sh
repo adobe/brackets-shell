99583eea9efe1fd0f57b76213876d94d60e5f5e0
Prashanth Nethi <prashant@adobe.com>
2015-04-14 22:36:37 +0530

    Incorporating some code view comments as well changing the precendene in the shell script to first look for app inside HOME/Application opposed to first checking for /Applications/Brackets presence, in case the enclosing app is missing.

--- appshell/mac/brackets.sh
+++ appshell/mac/brackets.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# The actual location of the symlink is passed
+# in $0. We first get the correct target from the
+# symlink and then go about finding the enclosing
+# app. Once a .app is found, we plainly pass the
+# location of the app to open command with a -a
+# as an argument. Note that readlink works differntly
+# on mac.
+
 TARGET_FILE=$0
 BRACKETS_APP_NAME="Brackets.app"
 
@@ -27,12 +35,15 @@ else
 fi
 
 # Give preference to launching the script's enclosing app
+# followed by the one in HOME/Applications. Last one
+# would be checked for global /Applications
+
 if [ -x "$FINAL_APP_PATH" ]; then
     open -a "$FINAL_APP_PATH" "$@"
-elif [ -x "/Applications/$BRACKETS_APP_NAME" ]; then
-    open -a /Applications/$BRACKETS_APP_NAME "$@"
 elif [ -x "$HOME/Applications/$BRACKETS_APP_NAME" ]; then
     open -a $HOME/Applications/$BRACKETS_APP_NAME "$@"
+elif [ -x "/Applications/$BRACKETS_APP_NAME" ]; then
+    open -a /Applications/$BRACKETS_APP_NAME "$@"
 else
     open -a Brackets "$@"
 fi
