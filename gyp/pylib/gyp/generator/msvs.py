a28fe08a4f3a61721dd85d4b66fe1031e6ecd680
ficristo <ficristo.work@gmail.com>
2016-04-02 18:13:10 +0200

    Reapply "Fix directory copy for gyp on Windows"
    This is the same fix of Glenn Ruehle of 18/06/2013
    on top of the update of gyp
    (SHA-1: a276d559aa147580f1f69d401cf4ae3c10ae4382)

--- gyp/pylib/gyp/generator/msvs.py
+++ gyp/pylib/gyp/generator/msvs.py
@@ -1687,7 +1687,7 @@ def _GetCopies(spec):
         src_bare = src[:-1]
         base_dir = posixpath.split(src_bare)[0]
         outer_dir = posixpath.split(src_bare)[1]
-        cmd = 'cd "%s" && xcopy /e /f /y "%s" "%s\\%s\\"' % (
+        cmd = 'xcopy /e /f /y "%s\\%s" "%s\\%s\\"' % (
             _FixPath(base_dir), outer_dir, _FixPath(dst), outer_dir)
         copies.append(([src], ['dummy_copies', dst], cmd,
                        'Copying %s to %s' % (src, dst)))
