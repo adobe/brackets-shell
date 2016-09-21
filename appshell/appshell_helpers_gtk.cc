8f2f6c2988d10544182cbf7a4d33c3e3a45545ba
ficristo <ficristo.work@gmail.com>
2016-09-21 17:59:33 +0200

    Complete appshell_helpers_gtk

--- appshell/appshell_helpers_gtk.cc
+++ appshell/appshell_helpers_gtk.cc
@@ -21,11 +21,21 @@
  *
  */
 
-#include "appshell_helpers.h"
+#include "appshell/appshell_helpers.h"
+
+#include <gtk/gtk.h>
+#include <sys/stat.h>
+
+#include "include/cef_version.h"
+#include "appshell/common/client_switches.h"
+#include "config.h"
+
+namespace appshell {
 
 extern char _binary_appshell_appshell_extensions_js_start;
 
-namespace appshell {
+static std::string szInitialUrl;
+static std::string szRunningDir;
 
 std::string GetExtensionJSSource()
 {
@@ -54,6 +64,112 @@ std::string GetExtensionJSSource()
 
 void PopulateSettings(CefSettings* settings) {
     settings->no_sandbox = true;
+}
+
+CefString AppGetProductVersionString() {
+    // TODO
+    return CefString("");
+}
+
+CefString AppGetChromiumVersionString() {
+    // TODO
+    return CefString("");
+}
+
+CefString AppGetSupportDirectory()
+{
+    gchar *supportDir = g_strdup_printf("%s/%s", g_get_user_config_dir(), APP_NAME);
+    CefString result = CefString(supportDir);
+    g_free(supportDir);
+
+    return result;
+}
+
+CefString AppGetDocumentsDirectory()
+{
+    const char *dir = g_get_user_special_dir(G_USER_DIRECTORY_DOCUMENTS);
+    if (dir == NULL)  {
+        return AppGetSupportDirectory();
+    } else {
+        return CefString(dir);
+    }
+}
+
+CefString AppGetCachePath() {
+    std::string cachePath = std::string(AppGetSupportDirectory()) + "/cef_data";
+
+    return CefString(cachePath);
+}
+
+std::string AppGetRunningDirectory() {
+  if(szRunningDir.length() > 0)
+    return szRunningDir;
+
+  char buf[512];
+  int len = readlink("/proc/self/exe", buf, 512);
+
+//  if(len < 0)
+//    return AppGetWorkingDirectory();  //# Well, can't think of any real-world case where this would be happen
+
+  for(; len >= 0; len--){
+    if(buf[len] == '/'){
+      buf[len] = '\0';
+      szRunningDir.append(buf);
+      return szRunningDir;
+    }
+  }
+}
+
+int GetInitialUrl(std::string& url) {
+    GtkWidget *dialog;
+    const char* dialog_title = "Please select the index.html file";
+    GtkFileChooserAction file_or_directory = GTK_FILE_CHOOSER_ACTION_OPEN;
+    dialog = gtk_file_chooser_dialog_new(dialog_title,
+                                         NULL,
+                                         file_or_directory,
+                                         GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+                                         GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT,
+                                         NULL);
+    if (gtk_dialog_run (GTK_DIALOG (dialog)) == GTK_RESPONSE_ACCEPT) {
+        url = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (dialog));
+        gtk_widget_destroy (dialog);
+        return 0;
+    }
+
+    return -1;
+}
+
+bool FileExists(std::string path) {
+    struct stat buf;
+    return (stat(path.c_str(), &buf) >= 0) && (S_ISREG(buf.st_mode));
+}
+
+int AppInitInitialUrl(CefRefPtr<CefCommandLine> command_line) {
+    if (command_line->HasSwitch(client::switches::kStartupPath)) {
+        szInitialUrl = command_line->GetSwitchValue(client::switches::kStartupPath);
+        return 0;
+    }
+
+    std::string url = AppGetRunningDirectory();
+    url.append("/dev/src/index.html");
+
+    if (!FileExists(url)) {
+        url = AppGetRunningDirectory();
+        url.append("/www/index.html");
+
+        if (!FileExists(url)) {
+            if (GetInitialUrl(url) < 0) {
+                return -1;
+            }
+        }
+    }
+
+    szInitialUrl = url;
+    return 0;
+}
+
+CefString AppGetInitialURL() {
+    return szInitialUrl;
 }
 
 }  // namespace appshell
