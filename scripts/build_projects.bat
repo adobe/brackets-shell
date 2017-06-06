066b0506b9ba92a6365346db73349362624a5256
Saurabh Kathpalia <kathpali@adobe.com>
2017-06-06 21:52:05 +0530

    Revert "Fixed VS2015 Compilation error"
    This reverts commit ad648a024352893231b370cdfc6bd6076d9cd5c2.

--- scripts/build_projects.bat
+++ scripts/build_projects.bat
@@ -1,23 +1,13 @@
 :: Batch file that sets up Visual Studio command line tools and builds appshell.sln
 :: This file should *not* be called directly. It is called by the build.sh script.
-@ECHO OFF
+ECHO OFF
 
-IF DEFINED VS140COMNTOOLS GOTO VS2015
-IF DEFINED VS120COMNTOOLS GOTO VS2013
 IF DEFINED VS110COMNTOOLS GOTO VS2012
 IF DEFINED VS100COMNTOOLS GOTO VS2010
 
 :VSNotInstalled
-ECHO Visual Studio 2010, 2012, 2013 or 2015 must be installed.
-GOTO Error
-
-:VS2015
-call "%VS140COMNTOOLS%/vsvars32.bat"
-GOTO Build
-
-:VS2013
-call "%VS120COMNTOOLS%/vsvars32.bat"
-GOTO Build
+ECHO Visual Studio 2010 or 2012 must be installed.
+GOTO Exit
 
 :VS2012
 call "%VS110COMNTOOLS%/vsvars32.bat"
@@ -28,9 +18,8 @@ call "%VS100COMNTOOLS%/vsvars32.bat"
 GOTO Build
 
 :Build
-msbuild.exe appshell.sln /nr:false /t:Clean /p:Platform=Win32 /p:Configuration=Release
-msbuild.exe appshell.sln /nr:false /t:Build /p:Platform=Win32 /p:Configuration=Release
-exit /b %ERRORLEVEL%
+msbuild.exe appshell.sln /t:Clean /p:Platform=Win32 /p:Configuration=Release
+msbuild.exe appshell.sln /t:Build /p:Platform=Win32 /p:Configuration=Release
 
-:Error
-exit /b 1
+:Exit
+exit /b %ERRORLEVEL%
