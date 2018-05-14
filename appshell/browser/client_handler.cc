7350d0b5fd1cf7f4ef156933ee17503644dae825
Gautam Jha <gautam0217@gmail.com>
2018-05-14 17:35:00 +0530

    Build fixes for Ubuntu18.04 (#649)

--- appshell/browser/client_handler.cc
+++ appshell/browser/client_handler.cc
@@ -337,6 +337,9 @@ bool ClientHandler::OnDragEnter(CefRefPtr<CefBrowser> browser,
 }
 #endif
 
+#ifndef OS_LINUX
+//We do not plan to add any feature to parent class(::ClientHandler) implementation of this function.
+//So override is useless, modern compilers will complain about this.
 void ClientHandler::OnDraggableRegionsChanged(
     CefRefPtr<CefBrowser> browser,
     const std::vector<CefDraggableRegion>& regions) {
@@ -344,6 +347,7 @@ void ClientHandler::OnDraggableRegionsChanged(
 
   NotifyDraggableRegions(regions);
 }
+#endif
 
 bool ClientHandler::OnRequestGeolocationPermission(
       CefRefPtr<CefBrowser> browser,
